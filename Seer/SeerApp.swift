387c5a4b3ab73e0bd627a04c4cdf4292fdf5d6d1
Jacob Davis <ismyhc@gmail.com>
2022-10-30 10:13:49 -0400

    Initial Commit

---
+++ Seer/SeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeerApp.swift
+//  Seer
+//
+//  Created by Jacob Davis on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
